0f3496ce150e8a27834420c17c34ae5cf29bb02b
Deniz Akcal <denizakcal@mail.com>
2018-01-26 19:00:26 -0500

    I added Races enum.

---
+++ include/Races.h
@@ -0,0 +1,16 @@
+#ifndef RACES_H
+#define RACES_H
+
+
+class Races
+{
+    public:
+        Races();
+        virtual ~Races();
+
+    protected:
+
+    private:
+};
+
+#endif // RACES_H
---
+++ src/Races.cpp
@@ -0,0 +1,3 @@
+#include "Races.h"
+
+enum class Races {AMAZONS, DWARVES, ELVES, GHOULS, GIANTS, HALFLINGS, HUMANS, ORCS, RATMEN, SKELETONS, SORCERERS, TRITONS, TROLLS, WIZARDS}; // I didn't put blank race, at least not yet.
