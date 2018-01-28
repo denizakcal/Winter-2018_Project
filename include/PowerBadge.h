e191ecb32ab80b73019153cfa602e18f4542bf0f
Deniz Akcal <denizakcal@mail.com>
2018-01-28 18:48:16 -0500

    I added the PowerBadge enum that you made.

---
+++ include/PowerBadge.h
@@ -0,0 +1,24 @@
+
+enum class PowerBadge {
+
+    COMMANDO,
+    ALCHEMIST,
+    FORTIFIED,
+    MERCHANT,
+    SPIRIT,
+    HILL,
+    BIVOUACKING,
+    BERSERK,
+    DIPLOMAT,
+    DRAGON_MASTER,
+    MOUNTED,
+    PILLAGING,
+    FLYING,
+    SEAFARING,
+    FOREST,
+    HEROIC,
+    STOUT,
+    SWAMP,
+    UNDERWORLD,
+    WEALTHY
+}
