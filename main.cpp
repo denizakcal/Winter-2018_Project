bea61d99cb93f83b2ec80f6768353e3854bf8734
Deniz Akcal <denizakcal@mail.com>
2018-01-27 19:26:09 -0500

    I amended main.cpp.

--- main.cpp
+++ main.cpp
@@ -13,20 +13,20 @@ int main()
 
     GraphOfRegions g;
     Regions regionsArray[2];
-    regionsArray[0] = Regions::REGION_TWO;
-    regionsArray[1] = Regions::REGION_THREE;
+    regionsArray[0] = Regions::MOUNTAIN_TWO;
+    regionsArray[1] = Regions::MOUNTAIN_THREE;
 
     std::vector<Regions> regionsVector;
-    regionsVector.push_back(Regions::REGION_TWO);
-    regionsVector.push_back(Regions::REGION_THREE);
+    regionsVector.push_back(Regions::MOUNTAIN_TWO);
+    regionsVector.push_back(Regions::MOUNTAIN_THREE);
 
-    g[Regions::REGION_ONE] = regionsVector;
+    g[Regions::MOUNTAIN_ONE] = regionsVector;
 
-    if( g[Regions::REGION_ONE].at(0) == Regions::REGION_ONE) {
+    if( g[Regions::MOUNTAIN_ONE].at(0) == Regions::MOUNTAIN_ONE) {
 
         std::cout << "First region";
     }
-    else if( g[Regions::REGION_ONE].at(0) == Regions::REGION_TWO) {
+    else if( g[Regions::MOUNTAIN_ONE].at(0) == Regions::MOUNTAIN_TWO) {
 
         std::cout << "Second region";
     }
