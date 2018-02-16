fe7d00951bfbb7980c1504ba00f2fa5ad5f7a15a
Deniz Akcal <denizakcal@mail.com>
2018-02-16 17:51:05 -0500

    I added the skeletons for Player.h and Player.cpp.

---
+++ src/Player.cpp
@@ -0,0 +1,18 @@
+/*
+ * Player.cpp
+ *
+ *  Created on: Feb 13, 2018
+ *      Author: deniz
+ */
+
+#include "Player.h"
+
+Player::Player() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Player::~Player() {
+	// TODO Auto-generated destructor stub
+}
+
