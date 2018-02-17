82e29eba2c2a4a0e9eb376c34e5ac9c35592a381
Deniz Akcal <denizakcal@mail.com>
2018-02-16 21:34:16 -0500

    My previous two commits for refactoring RegionPieces and Races enum classes just deleted the file, because I needed to re-add them, so now, they're officially added.

---
+++ src/Races.hpp
@@ -0,0 +1,17 @@
+enum class Races {
+	AMAZONS,
+	DWARVES,
+	ELVES,
+	GHOULS,
+	GIANTS,
+	HALFLINGS,
+	HUMANS,
+	ORCS,
+	RATMEN,
+	SKELETONS,
+	SORCERERS,
+	TRITONS,
+	TROLLS,
+	WIZARDS,
+	NO_RACE
+}; // I didn't put a blank race, at least not yet.
---
+++ src/RegionPieces.hpp
@@ -0,0 +1,11 @@
+enum class RegionPieces {
+
+    TROLL_LAIR,
+    FORTRESS,
+    MOUNTAIN,
+    ENCAMPMENT,
+    HOLES_IN_GROUND,
+    HERO,
+    DRAGON,
+	NO_REGION_PIECE
+}
