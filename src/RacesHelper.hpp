0b5f8196fd811ece24b41db8ca45eca3d74130a1
Deniz Akcal <denizakcal@mail.com>
2018-02-20 19:53:10 -0500

    I added RacesHelper.hpp.

---
+++ src/RacesHelper.hpp
@@ -0,0 +1,16 @@
+
+#ifndef RACESHELPER_HPP_
+#define RACESHELPER_HPP_
+
+#include "RacesHelper.hpp"
+
+#include <string>
+
+#include "Races.hpp"
+
+std::string enumToString(Races racesEnum);
+
+Races racesStringToEnum(std::string racesString);
+
+
+#endif /* RACESHELPER_HPP_ */
