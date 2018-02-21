14a273615ef9f66f15167b9002bbb9950c1e4473
Deniz Akcal <denizakcal@mail.com>
2018-02-20 19:52:42 -0500

    I added (skeleton) of RacesHelper.cpp.

---
+++ src/RacesHelper.cpp
@@ -0,0 +1,11 @@
+#include "RacesHelper.hpp"
+
+std::string enumToString(Races racesEnum) {
+
+	return "-1"; // temporary line
+}
+
+Races racesStringToEnum(std::string racesString) {
+
+	return Races::NO_RACE; // temporary line
+}
