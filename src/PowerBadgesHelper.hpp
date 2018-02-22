209468f3cc7a691d0b6c719f1b98a1cee8493292
Deniz Akcal <denizakcal@mail.com>
2018-02-22 17:10:58 -0500

    I added PowerBadgesHelper.hpp.

---
+++ src/PowerBadgesHelper.hpp
@@ -0,0 +1,11 @@
+
+#ifndef POWERBADGESHELPER_HPP_
+#define POWERBADGESHELPER_HPP_
+
+#include "PowerBadges.hpp"
+
+#include <string>
+
+PowerBadges powerBadgesStringToEnum(std::string powerBadgesString);
+
+#endif /* POWERBADGESHELPER_HPP_ */
