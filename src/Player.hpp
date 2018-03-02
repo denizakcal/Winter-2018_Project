701cba9f81ea10dac1966ab0d641bb83d848eef5
Deniz Akcal <denizakcal@mail.com>
2018-03-01 19:55:44 -0500

    I renamed Player.h to Player.hpp.

---
+++ src/Player.hpp
@@ -0,0 +1,17 @@
+/*
+ * Player.h
+ *
+ *  Created on: Feb 13, 2018
+ *      Author: deniz
+ */
+
+#ifndef PLAYER_HPP_
+#define PLAYER_HPP_
+
+class Player {
+public:
+	Player();
+	virtual ~Player();
+};
+
+#endif /* PLAYER_HPP_ */
