3ec4315ff3504638cd3090251f8ea7f7a69ab1e8
Deniz Akcal <denizakcal@mail.com>
2018-03-07 18:25:57 -0500

    I added some things to Player.cpp.

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,9 +1,21 @@
 
 #include "Player.hpp"
 
+#include <string>
+
 Player::Player() {
 	// TODO Auto-generated constructor stub
 
+}
+
+void Player::setName(std::string name) {
+
+	this->name = name;
+}
+
+void Player::setGraph(Graph* subGraph) {
+
+	this->subGraph = subGraph;
 }
 
 Player::~Player() {
