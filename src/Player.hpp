ea3d831e32ad82e91376c9fb6dd115317314b4cc
Deniz Akcal <denizakcal@mail.com>
2018-03-07 18:26:06 -0500

    I added some things to Player.hpp.

--- src/Player.hpp
+++ src/Player.hpp
@@ -2,10 +2,18 @@
 #ifndef PLAYER_HPP_
 #define PLAYER_HPP_
 
+#include <string>
+#include "Graph.hpp"
+
 class Player {
 public:
+	std::string name;
+	Graph* subGraph = nullptr;
+
 	Player();
 	virtual ~Player();
+	void setName(std::string name);
+	void setGraph(Graph* subGraph);
 };
 
 #endif /* PLAYER_HPP_ */
