10a2f4f7c80645d695c56b2d752482bf2f3a371e
Deniz Akcal <denizakcal@mail.com>
2018-03-11 18:18:54 -0400

    I implemented some semi-skeleton code for stuff related to UserInterface+TextualUserInterface.

--- src/TextualUserInterface.cpp
+++ src/TextualUserInterface.cpp
@@ -2,25 +2,43 @@
 
 void TextualUserInterface::displayCurrentSnapshotOfGame() {
 
-
+	std::cout << "The game is now resumed." << std::endl;
+	isPaused = false;
 }
 
 void TextualUserInterface::displayPauseScreen() {
 
-	// TODO
+	std::cout << "The game is now paused." << std::endl;
+	isPaused = true;
+
+	std::cout << "Here are your options.:" << std::endl;
+	std::cout << "1) Resume the game." << std::endl;
+	std::cout << "2) Save the game." << std::endl;
 }
 
 void TextualUserInterface::displayMainMenuScreen() {
 
-	// TODO
+	std::cout << "Welcome to the main menu." << std::endl;
+
+	std::cout << "Here are your options.:" << std::endl;
+	std::cout << "1) Start a new game." << std::endl;
+	std::cout << "2) Load an already-existing game." << std::endl;
 }
 
 void TextualUserInterface::displayMapSelectionScreen() {
 
-	// TODO
+	// isn't there only one map per amount of players? TODO
 }
 
 void TextualUserInterface::displayNumbefOfPlayersSelectionScreen() {
 
-	// TODO
+	std::cout << "Enter the amount of players that will be playing this game.:" << std::endl;
+	std::cin >> numberOfPlayers;
+
+	while( !(numberOfPlayers >= 2 && numberOfPlayers <= 5) ) {
+
+		std::cout << "An invalid amount of players has been entered." << std::endl;
+		std::cout << "Enter the amount of players that will be playing this game.:" << std::endl;
+		std::cin >> numberOfPlayers;
+	}
 }
--- src/UserInterface.hpp
+++ src/UserInterface.hpp
@@ -6,16 +6,20 @@
 #include "Graph.hpp"
 
 class UserInterface {
-public:
-	UserInterface();
-	virtual ~UserInterface();
-	TheGame loadGame(std::string fileName);
-	void saveGame(TheGame theGame);
-	virtual void displayCurrentSnapshotOfGame() = 0;
-	virtual void displayPauseScreen() = 0;
-	virtual void displayMainMenuScreen() = 0;
-	virtual void displayMapSelectionScreen() = 0;
-	virtual void displayNumbefOfPlayersSelectionScreen() = 0;
+	protected:
+		int numberOfPlayers = 0;
+		bool isPaused = false;
+
+	public:
+		UserInterface();
+		virtual ~UserInterface();
+		TheGame loadGame(std::string fileName);
+		void saveGame(TheGame theGame);
+		virtual void displayCurrentSnapshotOfGame() = 0;
+		virtual void displayPauseScreen() = 0;
+		virtual void displayMainMenuScreen() = 0;
+		virtual void displayMapSelectionScreen() = 0;
+		virtual void displayNumbefOfPlayersSelectionScreen() = 0;
 };
 
 #endif /* USERINTERFACE_H_ */
